ad422f249e4f43aa971e5cc85fbbe2ea6e6c1b33
tokghwt <tokghwt@gmail.com>
2023-06-20 17:55:54 +0900

    chore: Install typescript-eslint

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,4 +1,5 @@
 // https://eslint.org/docs/latest/use/configure/
+// https://typescript-eslint.io/getting-started/
 module.exports = {
   root: true,
   env: {
@@ -7,8 +8,16 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
+    // https://typescript-eslint.io/architecture/parser/#configuration
+    project: "./typescript/tsconfig.json",
   },
+  plugins: [
+    "@typescript-eslint",
+  ],
 };
