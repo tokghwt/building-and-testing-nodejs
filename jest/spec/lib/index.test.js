4a6829e3f92a387edd61e1055f8f63b623fbcf41
itou0104jp <itou0104jp@gmail.com>
2023-06-29 17:27:41 +0900

    add API greet2

--- jest/spec/lib/index.test.js
+++ jest/spec/lib/index.test.js
@@ -1,5 +1,9 @@
-const { greet } = require('../../../lib/index');
+const { greet, greet2 } = require('../../../lib/index');
 
 test('greet "everyone"', () => {
   expect(greet('everyone')).toBe('Hello everyone!');
+});
+
+test('greet2 "everyone"', () => {
+  expect(greet2('everyone')).toBe('Hi everyone!');
 });
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,7 @@
 export function greet(person: string) {
   return `Hello ${person}!`;
 }
+
+export function greet2(person: string) {
+  return `Hi ${person}!`;
+}
